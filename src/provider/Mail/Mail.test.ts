d797252fb67ae6f1052284174f53d2ffa7c79bf8
João Pedro <jpmns9103@gmail.com>
2021-11-17 18:48:37 -0300

    criação da classe provedora de email, instalação de suas dependências e criação dos corpos de email

---
+++ src/provider/Mail/Mail.test.ts
@@ -0,0 +1,15 @@
+jest.setTimeout(20000);
+
+import Mail from "./Mail";
+const mail = new Mail();
+
+describe("Email Send", () => {
+
+	test("Should send the email", async () => {
+		await mail.sendMail("joao@teste.com", "Test", "createUserEmailBody", {
+			appUrl: "test",
+			email: "test",
+			token: "test"
+		});
+	});
+});
