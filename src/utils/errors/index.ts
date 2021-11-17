27fd20efb5734ac0d90f573ef24c49c1f82697e9
João Pedro <jpmns9103@gmail.com>
2021-11-16 23:08:18 -0300

    criação de erros personalizados

---
+++ src/utils/errors/index.ts
@@ -0,0 +1,11 @@
+import InvalidParamError from "./InvalidParamError";
+import MissingParamError from "./MissingParamError";
+import UnauthorizedError from "./UnauthorizedError";
+import NotAllowedCors from "./NotAllowedCors";
+
+export {
+	InvalidParamError,
+	MissingParamError,
+	UnauthorizedError, 
+	NotAllowedCors
+};
